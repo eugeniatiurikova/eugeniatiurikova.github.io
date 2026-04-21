bde9f9bb5e57fbb8e7b229eb47180582ee5f2b52
eugeniatiurikova <89794249+eugeniatiurikova@users.noreply.github.com>
2026-04-21 17:38:40 -0300

    with changeable configs

---
+++ cyrillic-languages/content/about_en.js
@@ -0,0 +1,23 @@
+/**
+ * Text for the "About" page in English.
+ *
+ * Edit the HTML content between the backticks ( ` ) below.
+ *
+ * IMPORTANT:
+ *   - Do NOT use the ` (backtick) character inside the text.
+ *   - Do NOT use the sequence ${ inside the text.
+ *     If you need a literal $, write \$ instead, or use &#36; in HTML.
+ */
+window.__APP_HTML = window.__APP_HTML || {};
+window.__APP_HTML.about_en = `
+<p class="langtitle">About the project</p>
+<p class="langinfo_next redtext"><br />This is a&nbsp;knowledge base that contains about 80&nbsp;Cyrillic-based Languages.<br />It can help designers to develop and localize fonts.</p>
+<p class="langinfo_next">The project has existed since the&nbsp;2000s and&nbsp;is&nbsp;constantly improving, considering changes in&nbsp;the&nbsp;Languages’ alphabets and&nbsp;their Unicode support.</p>
+<p class="langinfo_next">The knowledge base has not&nbsp;only current alphabets but&nbsp;also other characters needed to&nbsp;support the&nbsp;Languages. For&nbsp;instance, for&nbsp;the&nbsp;Abkhazian language, there are&nbsp;historical and&nbsp;dialect glyph forms in&nbsp;addition to&nbsp;the&nbsp;alphabet. They can&nbsp;come useful in&nbsp;historical books, textbooks, and&nbsp;reference books.</p>
+<p class="langinfo_next">When browsing a&nbsp;Language, you can&nbsp;choose the&nbsp;desired font type (serif or&nbsp;sans-serif), case (set of&nbsp;uppercase or&nbsp;lowercase characters), and&nbsp;font style (upright or&nbsp;italic).</p>
+<p class="langinfo_next">The aggregated Cyrillic alphabet that supports all&nbsp;Languages in&nbsp;this project can&nbsp;be&nbsp;found on&nbsp;the&nbsp;<span class="aspan" data-action="pancyr">Pan-Cyrillic Character&nbsp;set</span>&nbsp;page.</p>
+<p class="langinfo_next">As a&nbsp;reference material, this project can be&nbsp;useful for font designers and anyone interested in&nbsp;Cyrillic.</p>
+<p class="langinfo_next">The project uses a&nbsp;special version of the <a href="https://github.com/paratype/paratype.github.io/tree/main/cyrillic-languages/fonts/web" target="top">PT Sans Expert and PT Serif Expert</a> fonts (Paratype, 2022, OFL).</p>
+<p class="langinfo_next">We’ll be&nbsp;grateful <a href="mailto:fonts@paratype.com">for your comments</a>. They will help us&nbsp;make the&nbsp;project better.</p>
+<p class="langinfo">&nbsp;</p>
+`;
